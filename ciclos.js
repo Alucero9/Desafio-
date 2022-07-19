23e10b4c1b65e305d7dcd6a54fed2de94d155e78
Andres Lucero <luceroandresignacio@gmail.com>
2022-07-18 21:48:59 -0300

    desafio

---
+++ ciclos.js
@@ -0,0 +1,8 @@
+let numero = prompt("Ingresar un numero");
+let texto = ("hola" + " ")
+
+while(numero != "ESC" ){
+    alert(texto .repeat(numero));
+   
+    numero = prompt("Ingresar otro numero");
+}
